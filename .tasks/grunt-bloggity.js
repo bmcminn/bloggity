34cc3caf006c65bbe6a1da4d50a058cc41246d91
bmcminn <labs@gbox.name>
2015-11-29 05:05:50 -0600

    whole mess of updates; we're finally rendering pages!; schema is looking good and generated folder structure and content strategy appears sound

--- .tasks/grunt-bloggity.js
+++ .tasks/grunt-bloggity.js
@@ -3,7 +3,7 @@
 var path        = require('path')
   , fs          = require('fs')
   , _           = require('lodash')
-  , pygmentize  = require('pygmentize-bundled')
+  // , pygmentize  = require('pygmentize-bundled')
   , marked      = require('marked')
   , RSS         = require('rss')
   , fastmatter  = require('fastmatter')
@@ -12,7 +12,7 @@ var path        = require('path')
 
 JSON.minify = require('jsonminify');
 
-
+// build collection of template engines and their various implementation details
 var templateEngines = {
   jade: {
     engine: require('jade'),
@@ -21,13 +21,24 @@ var templateEngines = {
 };
 
 
-var config = {}
-  , files = {
+// build collection of file locations
+var files = {
       bloggityJSON: path.resolve('.', 'bloggity.json')
+    , appCacheJSON: path.resolve('.', '.grunt','cache','bloggity.json')
     }
   ;
 
 
+// initialize a temp storage variable
+var temp;
+
+
+
+/**
+ * [exports description]
+ * @param  {[type]} grunt [description]
+ * @return {[type]}       [description]
+ */
 module.exports = function(grunt) {
 
   'use strict';
@@ -43,19 +54,40 @@ module.exports = function(grunt) {
     return JSON.parse(JSON.minify(content));
   };
 
+  /**
+   * [writeJSON description]
+   * @param  {[type]} targetLoc [description]
+   * @param  {[type]} data      [description]
+   * @param  {[type]} pretty    [description]
+   * @return {[type]}           [description]
+   */
+  grunt.file.writeJSON = function(targetLoc, data, pretty) {
+    pretty = pretty || 2;
+    grunt.file.write(targetLoc, JSON.stringify(data, null, pretty));
+  };
 
-  // Initialize the app instance
-  var app = {
-        model: {}
-      }
-    ;
 
   // Allow for test objects to be used during unit testing
-  var _this   = grunt.testContext || {};
-  var options = grunt.testOptions || {};
+  var _this   = grunt.testContext || {}
+    , options = grunt.testOptions || {}
+    ;
 
-  // Create a reference to the template engine that is available to all library methods
-  var templateEngine;
+
+  // Initialize the app instance
+  var app = grunt.file.exists(files.appCacheJSON) ? grunt.file.readJSON(files.appCacheJSON) : {};
+
+
+  // initialize our app config instance
+  app.config = {};
+
+
+  // init the pages collection
+  app.pages = [];
+
+
+  // get the current cache date
+  app.cacheDate = new Date();
+
 
   // Declare a global function to format URLs that is available to all library methods
   var formatPostUrl = function (urlSegment) {
@@ -85,69 +117,217 @@ module.exports = function(grunt) {
       // determine if the user has a cabin.json file or bloggity.json file
       if (!grunt.file.exists(files.bloggityJSON)) {
         grunt.fatal('You need to create a bloggity.json file.');
-
       } else {
-        config = _.merge(config, grunt.file.readJSON(files.bloggityJSON));
-
+        app.config = _.merge(app.config, grunt.file.readJSON(files.bloggityJSON));
       }
 
-      // // initialize the posts object
-      // var posts = {};
-      // var numPosts = grunt.file.expand({
-      //       filter: 'isFile',
-      //       cwd: this.data.src
-      //     }, [
-      //       '**'
-      //     ]).length
+
+      // if post type already exists then kill the app and warn the user
+      if (temp = app.checkForDuplicatePostTypes()) {
+        grunt.fatal('postType(s) "' + temp.join('", "') + '" already exist in ' + files.bloggityJSON);
+      }
 
 
-      // iterate over each post type and fire off the necessary compilation event
-      _.each(config.postTypes, function(postType, index) {
-
-        // collect all postType files
-        var posts = {
-              files: grunt.file.expand(
-                  {
-                    filter: 'isFile'
-                  }
-                , path.resolve(postType.name.toLowerCase(), '**')
-                )
-            , posts: []
-            }
-          ;
-
-        // get file and it's contents
-        _.map(posts.files, function(filepath, index) {
-          var fileData = {
-                originalContent: grunt.file.read(filepath)
-              }
-            ;
-
-          // get post file content and parse YAML frontmatter config
-          fileData = _.merge(fileData, fastmatter(fileData.originalContent));
-
-          // merge file data into posts object
-          posts.posts.push(fileData);
-
-        });
+      // Create a reference to the template engine that is available to all library methods
+      app.templateEngine = templateEngines[app.config.templateEngine || 'jade'];
 
 
+      // update app site model
+      app.site = _.merge({
+        baseUrl: "http://localhost:3005"
+
+      }, app.config);
 
 
+      //
+      // APP RUNTIME
+      //
 
-        app.model[postType.name] = posts;
+      // get all postType files
+      app.getPostTypes();
 
-        // console.log(postType.name);
-        // console.log(posts);
+      // get all pages
+      app.getPages();
 
-      });
+      // save a snapshot of the current site model
+      app.cacheAppState();
 
 
-      console.log(JSON.stringify(app, null, 2));
+      app.renderPages();
+
+      // TODO: generate page/index.html files
+      // TODO: generate atom.xml
+      // TODO: generate sitemap.xml
+      // TODO: generate 404.html
+      // TODO: generate index.html
+      // TODO: generate post-listings files (ie: blog.html)
 
 
     });
 
 
-};
 
+    //
+    // APP MODULE DEFINITIONS
+    //
+
+    /**
+     * Writes out the model cache file of current app state
+     * @return {null}
+     */
+    app.cacheAppState = function() {
+      grunt.file.writeJSON(files.appCacheJSON, this);
+      return null;
+    };
+
+
+
+    /**
+     * [checkForDuplicatePostTypes description]
+     * @param  {[type]} postType [description]
+     * @return {[type]}          [description]
+     */
+    app.checkForDuplicatePostTypes = function() {
+
+      var postTypes = this.config.postTypes
+        , tests = []
+        ;
+
+      // test each postType for duplicate configs
+      _.map(postTypes, function(n) {
+        var dups = _.pluck(_.filter(postTypes, { 'name': n.name }), 'name');
+        if (dups.length > 1) { tests.push(dups[0]); }
+      });
+
+      // if we found duplicate names, return the names
+      if (tests.length > 0) {
+        return _.uniq(tests);
+
+      } else {
+        return false;
+
+      }
+    };
+
+
+
+    /**
+     * [buildFileData description]
+     * @param  {[type]} fileCollection   [description]
+     * @return {[type]}                  [description]
+     */
+    app.buildFileData = function(fileCollection) {
+
+      var results = []
+        , fileData
+        ;
+
+      // iterate over each page
+      _.each(fileCollection, function(filepath, index) {
+
+        fileData = path.parse(filepath.toLowerCase());
+
+        fileData.filepath = filepath;
+        fileData.originalContent = grunt.file.read(filepath);
+
+        fileData = _.merge(
+          fileData
+        , fastmatter(fileData.originalContent)
+        , fs.statSync(filepath)
+        );
+
+        //
+        results.push(fileData);
+
+      });
+
+      return results;
+
+    };
+
+
+
+    /**
+     * [getPostTypes description]
+     * @return {[type]} [description]
+     */
+    app.getPostTypes = function() {
+
+      var self      = this
+        , postTypes = this.config.postTypes
+        ;
+
+      // iterate over each post type and generate their part of the site model
+      _.each(postTypes, function(postType, index) {
+
+        // normalize the post type name string
+        postType.name = postType.name.toLowerCase();
+
+        // collect all postType files
+        var postFiles = grunt.file.expand({ filter: 'isFile' }, path.resolve(postType.src, '**'))
+          ;
+
+        // initialize the postType data object
+        app[postType.name] = self.buildFileData(postFiles);
+      });
+
+      return false;
+    };
+
+
+
+    /**
+     * [getPages description]
+     * @return {[type]} [description]
+     */
+    app.getPages = function() {
+
+      var self      = this
+        , pages     = this.config.pages
+        , pageFiles = grunt.file.expand({ filter: 'isFile' }, path.resolve(pages.name, '**'))
+        ;
+
+      // reassign our pages collection back to the app instance
+      this.pages = this.buildFileData(pageFiles);
+
+      return false;
+    };
+
+
+
+
+    app.renderPages = function() {
+
+      var self  = this
+        , pages = this.pages
+        , config = this.config
+        , templateEngine = this.templateEngine.engine
+        ;
+
+      // iteratate over each page
+      _.each(pages, function(page, index) {
+
+        self.filename = page.filepath;
+
+
+        // render content
+        page.renderedContent = templateEngine.render(page.body, self);
+
+        // find the render path for the file
+        if (page.filepath.match(/(index|\d{3,4})\.jade/)) {
+          page.renderPath = path.resolve(config.dest, page.name + '.html');
+        } else {
+          page.renderPath = path.resolve(config.dest, page.name, 'index.html');
+        }
+
+        grunt.file.write(page.renderPath, page.renderedContent);
+
+        console.log(page);
+      });
+
+
+      return false;
+    };
+
+
+};
