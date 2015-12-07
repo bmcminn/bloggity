274ba81629a981f4340bc4c5b93d06e050b41c06
bmcminn <labs@gbox.name>
2015-12-07 00:31:21 -0600

    added todo list generator

--- .tasks/grunt-todolist.js
+++ .tasks/grunt-todolist.js
@@ -2,11 +2,19 @@ module.exports = function(grunt) {
 
   'use strict';
 
-  var path  = require('path')
-    , _     = require('lodash')
+  var path    = require('path')
+    , _       = require('lodash')
+    , os      = require('os')
+    , moment  = require('moment')
 
-    , todo  = {}
-    , temp  = {}
+    , todo    = {}
+    , temp    = {}
+    , message = []
+
+    , paths = {
+        todoList: path.resolve('.', 'todo.md')
+      , nodeModules: path.resolve('.', 'node_modules', '**')
+      }
     ;
 
 
@@ -16,6 +24,12 @@ module.exports = function(grunt) {
    */
   grunt.registerTask('todolist', 'builds a todo list for the current project', function() {
 
+    // setup messaging
+    message.push('# TODO LIST');
+    message.push('> Date: ' + moment().format('MMMM DD, YYYY'));
+    message.push('')
+
+
     // setup our list collection
     todo.list = [];
 
@@ -24,27 +38,54 @@ module.exports = function(grunt) {
     todo.files = grunt.file.expand({
         filter: 'isFile'
       },
-      path.resolve('.', '**'),
-      '!' + path.resolve('.', 'node_modules', '**')
+      path.resolve('.', '**')
+    , '!' + paths.nodeModules
+    , '!' + paths.todoList
     );
-
-
-    console.log(todo.files);
 
 
     // iterate over each file we found
     _.each(todo.files, function(file, index) {
 
+      // read file contents
       temp.content = grunt.file.read(file);
 
+      // check file contents for all matches to TODO
+      temp.todos = temp.content.match(/TODO[:\s]*?([\s\S]+?)(?:[\r\n])/g);
 
-      console.log(temp.content);
+      // if we found TODOs
+      if (temp.todos) {
 
-      // TODO: add check for all instances of TODO*
+        // get the current working directory and split the string
+        temp.root = process.cwd().split(path.sep);
 
+        // get the current file path and split its string
+        temp.filepath = file.split('/');
+
+        // remove the first part of the path using the root dir length and add '.' to the first index
+        temp.filepath.splice(0, temp.root.length, '.');
+
+        // rebuild the reference file path
+        temp.filepath = temp.filepath.join('/');
+
+        // add the heading of the current file we're parsing
+        message.push('## ' + temp.filepath);
+        message.push('');
+
+        // iterate over ALL the todos we found and format that as a list of changes to each file
+        _.each(temp.todos, function(todo, index) {
+          message.push('- ' + todo.replace(/TODO[\:\s]*/, ''));
+        });
+
+      }
     });
 
-    return false;
+
+    // build message
+    message = message.join(os.EOL);
+
+    // write to todo.md
+    grunt.file.write(paths.todoList, message);
 
   });
 
---
+++ todo.md
@@ -0,0 +1,9 @@
+# TODO LIST
+> Date: December 07, 2015
+
+## ./bloggity.json
+
+- configure :postDate; "url": "blog/:postDate-:title/",
+## ./templates/archives.jade
+
+- fix pagination group section
